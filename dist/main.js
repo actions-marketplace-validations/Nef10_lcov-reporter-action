fd52f0e63063fe71da6889eb07f90386c5ea100c
Node Builder <217586+NodeBuilder[bot]@users.noreply.github.com>
2024-09-24 04:59:36 +0000

    npm run build

--- dist/main.js
+++ dist/main.js
@@ -648,7 +648,6 @@ function md5ii(a, b, c, d, x, s, t) {
 }
 
 var v3 = v35('v3', 0x30, md5);
-var v3$1 = v3;
 
 function v4(options, buf, offset) {
   options = options || {};
@@ -766,7 +765,6 @@ function sha1(bytes) {
 }
 
 var v5 = v35('v5', 0x50, sha1);
-var v5$1 = v5;
 
 var nil = '00000000-0000-0000-0000-000000000000';
 
@@ -784,9 +782,9 @@ var esmBrowser = /*#__PURE__*/Object.freeze({
 	parse: parse$4,
 	stringify: stringify$1,
 	v1: v1,
-	v3: v3$1,
+	v3: v3,
 	v4: v4,
-	v5: v5$1,
+	v5: v5,
 	validate: validate,
 	version: version
 });
